ebea14a2ad5a948f6f378bf549e3934383f85ce8
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2021-04-02 06:28:05 -0400

    setting up server

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/fitness',
     {
@@ -7,4 +20,7 @@ mongoose.connect(
       useFindAndModify: false
     }
   );
-  
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
