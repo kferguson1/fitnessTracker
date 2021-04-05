2e53a9ee5863093cda68bc2a6b94f719ad660a79
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2021-04-05 00:38:54 -0400

    added GET fuctions

--- server.js
+++ server.js
@@ -2,14 +2,67 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const path = require("path");
+
 const PORT = process.env.PORT || 3000;
 
+const db = require("./models");
 const app = express();
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
+app.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+app.post("/api/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+   .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.put("/api/workouts/:id", ({ body, params }, res) => {
+  db.Workout.updateOne({ _id: params.id }, { $push: { exercises: body }})
+   .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/api/workouts", (req, res) => {
+  db.Workout.find({}).sort({ day: -1 }).limit(1)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
 
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/fitness',
