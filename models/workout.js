1d99cb92572324371df6a6836fd4ea0b09c210ec
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2021-04-05 00:33:44 -0400

    added workout schemas

---
+++ models/workout.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: {
+      type: Date,
+      default: Date.now
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter Exercise Type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter Exercise Name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter Exercise Duration (in minutes)"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
+        }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include virtual properties (if any) when data is requested
+      virtuals: true
+    }
+  }
+);
+
+// adds dynamically-created property to schema
+workoutSchema.virtual("totalDuration").get(function() {
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
